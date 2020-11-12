b6521f97790f2e39fccfab2b8cc6287c71947c9b
spoorthi222 <44644365+spoorthi222@users.noreply.github.com>
2020-11-12 17:39:54 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+#include "student.c"
+#include <assert.h>
+
+
+int main(int argc, char *argv[]) {
+	int choice;
+	int accept;
+	Student stu[TOTAL_STUDENT];
+	accept=declaration();
+	assert(accept==1);
+
+	printf("\nADMISSION PROCESS\n");
+	printf("1. DECLARE TOTAL SEATS IN PROGRAM\n\n");
+    printf("2. STUDENT REGISTRATION\n\n");
+    printf("3. TOTAL FILLED SEATS IN PROGRAM\n\n");
+	printf("4. STUDENT INFORMATION\n\n");
+	printf("5. WAITING STUDENTS DETAILS\n\n");
+	printf("6. UPDATE WAITING STUDENT DETAILS\n\n");
+	printf("0. TO EXIT\n\n");
+	do{
+	    printf("\nCHOOSE FROM ABOVE OPTIONS:\n");
+	    scanf("%d",&choice);
+	    switch(choice){
+
+	    	case 1: manage_seats();
+			break;
+
+			case 2:	student_reg(stu);
+		    break;
+
+			case 3: display_total_filled_seats();
+			break;
+
+			case 4: display(stu);
+			break;
+
+			case 5: display_waiting_students(stu);
+			break;
+
+			case 6: waiting_students(stu);
+			break;
+
+			default: assert(choice<7);
+
+			}
+
+
+	}while(choice!=0);
+
+	return 0;
+}
